dccb6fda36d73719c277d1cf4f79be0d757c0f8b
Sanjay Soni <meatsit2017@gmail.com>
2021-11-07 22:47:08 +0530

    remove empty todos on uodate or add

--- lib/domain/usecase/add_note_usecase.dart
+++ lib/domain/usecase/add_note_usecase.dart
@@ -1,6 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 
+import 'package:note_app/common/extension/string.dart';
+
 import 'package:note_app/data/dto/note_dto.dart';
 import 'package:note_app/domain/model/error.dart';
 import 'package:note_app/domain/model/note.dart';
@@ -15,6 +17,7 @@ class AddNoteUsecase {
 
   Future<Either<NoteError, Unit>> call(Note note) async {
     try {
+      note.todo.removeWhere((todo) => todo.title.isEmptyString);
       final noteDto = NoteDto.fromNote(note);
 
       if (!noteDto.validNote) {
